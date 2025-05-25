d19e8b974fe7d7a283a3e715e88da48a095cfa24
Shivam Arora <shivamarora1@yahoo.com>
2025-05-25 20:13:16 +0530

    init commit

---
+++ backend/app/main.py
@@ -0,0 +1,73 @@
+import asyncio
+import logging
+from typing import Union
+import logfire
+from logfire import ConsoleOptions
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+
+from app.mcp_clients import scrape_website_from_similarweb
+
+logfire.configure(
+    send_to_logfire=False,
+    console=ConsoleOptions(
+        colors="auto",
+        include_timestamps=True,
+        include_tags=True,
+        verbose=True,
+    ),
+)
+logfire.instrument_pydantic_ai(event_mode="logs")
+logfire.instrument_httpx(capture_all=True)
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=getattr(logging, "INFO", logging.INFO),
+    format="%(asctime)s :: %(levelname)s :: %(processName)s :: %(threadName)s :: %(filename)s :: %(funcName)s :: %(message)s",
+)
+app = FastAPI()
+
+DATA_FETCH_TIMEOUT = 300
+
+
+@app.get("/scrape/{website_name}")
+async def scrape_website(website_name: str, q: Union[str, None] = None):
+    logger.info(f"Starting scraping for: {website_name}")
+    try:
+        tasks = {
+            website_name: asyncio.create_task(
+                scrape_website_from_similarweb(website_name)
+            ),
+        }
+
+        done, pending = await asyncio.wait(tasks.values(), timeout=DATA_FETCH_TIMEOUT)
+
+        results = {}
+        for key, task in tasks.items():
+            if task in done:
+                try:
+                    results[key] = await task
+                    logger.info(f"{key} task completed")
+                except Exception as e:
+                    results[key] = None
+                    logger.warning(f"{key} failed: {e}")
+            else:
+                task.cancel()
+                results[key] = None
+                logger.warning(f"{key} task timed out and was cancelled")
+
+        company_result = results[website_name]
+
+        logger.info(f"Result {company_result}")
+
+        return []
+
+    except Exception as e:
+        logger.error(f"Error analyzing company {website_name}: {str(e)}", exc_info=True)
+        raise HTTPException(
+            status_code=500,
+            detail=f"An error occurred while analyzing the company: {str(e)}",
+        )
---
+++ backend/app/mcp_clients.py
@@ -0,0 +1,71 @@
+import asyncio
+import logging
+from typing import List
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStdio
+import os
+from pydantic_ai.exceptions import ModelHTTPError
+from pydantic_ai.settings import ModelSettings
+
+load_dotenv()
+REQUEST_TIMEOUT = 240
+logger = logging.getLogger(__name__)
+
+
+class CompanyDescription(BaseModel):
+    description: str
+    category: str
+    year_founded: str
+    employees: str
+    annual_revenue: str
+    global_rank: str
+    visits: str
+    bounce_rate: str
+    avg_visit_duration: str
+    competitors: List[str]
+
+
+similarweb_scrapper_server = MCPServerStdio(
+    command="npx",
+    args=["@brightdata/mcp"],
+    env={
+        "API_TOKEN": os.getenv("API_TOKEN", ""),
+        "WEB_UNLOCKER_ZONE": os.getenv("WEB_UNLOCKER_ZONE", ""),
+        "BROWSER_AUTH": os.getenv("BROWSER_AUTH", ""),
+    },
+)
+
+similarweb_scrapper_agent = Agent(
+    "openai:gpt-4.1-mini",
+    output_type=CompanyDescription,
+    mcp_servers=[similarweb_scrapper_server],
+    retries=1,
+    system_prompt="Strictly follow these steps:"
+    "1. `scraping_browser_navigate`: To navigate to given url"
+    "2. `scraping_browser_get_text`: To extract textual content",
+    model_settings=ModelSettings(request_timeout=REQUEST_TIMEOUT, max_tokens=2048),
+)
+
+
+async def scrape_website_from_similarweb(website: str):
+    prompt = f"website: https://www.similarweb.com/website/{website}"
+
+    try:
+        async with similarweb_scrapper_agent.run_mcp_servers():
+            result = await asyncio.wait_for(
+                similarweb_scrapper_agent.run(prompt), timeout=REQUEST_TIMEOUT
+            )
+            logger.info(f"successfully scrapped data for {website}")
+            logger.info(result.output)
+            return result.output
+    except ModelHTTPError as e:
+        logger.error(f"API error for {website}: {str(e)}", exc_info=True)
+        return None
+    except Exception as e:
+        logger.error(
+            f"Unexpected error fetching data for {website}: {str(e)}",
+            exc_info=True,
+        )
+        return None
